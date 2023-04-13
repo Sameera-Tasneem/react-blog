f26e6f568b0d68a353b35ac6fff35fb889e40c0d
Sameera-Tasneem <60311547+Sameera-Tasneem@users.noreply.github.com>
2023-04-13 17:05:58 +0530

    error 404

---
+++ pages/404.js
@@ -0,0 +1,6 @@
+export default function Custom404() {
+    return (<>
+    <h1>404 - Page Not Found</h1>
+    <h2>Please check for correct URL or the site you're trying to access is taken down.</h2>
+    /</>
+  )}
